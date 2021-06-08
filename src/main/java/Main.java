c65d6a3f5f07597577364d656a1b1828874d8605
Artem <riddle1994@ya.ru>
2021-06-08 17:52:04 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static final String FACTS_URI =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(FACTS_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        List<Fact> input = mapper.readValue(
+                response.getEntity().getContent(),
+                new TypeReference<List<Fact>>() {});
+        input.stream()
+                .filter(s -> s.getUpvotes() > 0)
+                .forEach(System.out::println);
+        }
+    }
+
